527b91308e39c045515063d78cd0cd3b0ec32ccb
Erikmoraless <25402080218397@cecytebc.edu.mx>
2026-04-13 04:35:45 -0700

    Operacionesbasicas.py

--- Operacionesbasicas.py
+++ Operacionesbasicas.py
@@ -1,17 +1,3 @@
-# Pedir datos
-n1 = int(input("Introduce el primer numero: "))
-n2 = int(input("Introduce el segundo numero: "))
-
-# Realizar operaciones
-rs = n1 + n2
-rr = n1 - n2
-rm = n1 * n2
-
-# Mostrar resultados
-print("Resultado de la suma:", rs)
-print("Resultado de la resta:", rr)
-print("Resultado de la multiplicacion:", rm)
-
 # Division
 if n2 != 0:
     rd = n1 / n2
