882099825cfcfd3fb4b9020b889a4a8bdd19a5e2
Erikmoraless <25402080218397@cecytebc.edu.mx>
2026-04-15 22:07:13 -0700

    Delete Módulo 3.cpp

--- Módulo 3.cpp
+++
@@ -1,7 +0,0 @@
-    // Mostrar resultado de las operaciones
-    cout << "Resultado de la suma: " << rs << endl;
-    
-    cout << "Resultado de la resta: " << rr << endl;
-    
-    cout << "Resultado de la multiplicacion: " << rm << endl;
-
