0cef5eb869b0143a8563974e2dc883f10c7b4210
Erikmoraless <25402080218397@cecytebc.edu.mx>
2026-04-15 21:39:03 -0700

    Update and rename Operacionesbasicas.cpp to Módulo 3.cpp

---
+++ Módulo 3.cpp
@@ -0,0 +1,7 @@
+    // Mostrar resultado de las operaciones
+    cout << "Resultado de la suma: " << rs << endl;
+    
+    cout << "Resultado de la resta: " << rr << endl;
+    
+    cout << "Resultado de la multiplicacion: " << rm << endl;
+
